a4acc88e1bb31c9ba7e338aaeb0a110e9dbbad69
devxuri <suryaanshgm@gmail.com>
2025-09-03 20:59:57 +0100

    Firebase auth setup & more
    - Firebase auth established
    - Setup login and signup components
    - Setup basic routing with protected checks
    - Initial tailwind boilerplate set

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      You are logged in
+
+      <div className='w-full flex items-center justify-center mt-10'>
+        <p className='text-sm font-normal text-gray-400'>
+          <a href='/login'>Log In</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className='w-full flex items-center justify-center mt-10'>
+        <p className='text-sm font-normal text-gray-400'>
+          <a href='/signup'>Sign Up</a>
+        </p>
+      </div>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './components/Login';
+import Signup from './components/Signup';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import AuthRoute from './AuthRoute';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthRoute><App /></AuthRoute>} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+
   </React.StrictMode>
 );
 
