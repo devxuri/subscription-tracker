160e102925a5862ecb8239770fb1aa86a82c4f8c
devxuri <suryaanshgm@gmail.com>
2025-10-07 21:00:38 +0100

    ops: add admin and migrate
    - load models onto admin interface for django
    - update migration file to latest

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import *
+from api.models.category import Category
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Subscription)
