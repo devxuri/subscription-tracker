ee3224a8648080f69ad72bae2501605a94faf4d2
devxuri <suryaanshgm@gmail.com>
2025-10-15 22:14:01 +0100

    ops: update migrate and subscription model
    - new migration update
    - unify variable require behaviour between model and admin panel

--- backend/api/models/subscription.py
+++ backend/api/models/subscription.py
@@ -23,9 +23,9 @@ class Subscription(models.Model):
     currency = models.CharField(max_length=10)  # gbp, usd, etc.
     price = models.DecimalField(max_digits=10, decimal_places=2)  # price amount
     payment_date = models.DateField()  # date of payment
-    cycle = models.CharField(max_length=20, choices=CYCLE_CHOICES)  # daily, weekly, monthly, annually
-    frequency = models.IntegerField()  # number (paired with cycle)
-    recurring = models.BooleanField(default=True)  # yes/no
+    cycle = models.CharField(max_length=20, choices=CYCLE_CHOICES, null=True, blank=True)  # daily, weekly, monthly, annually
+    frequency = models.IntegerField(null=True, blank=True)  # number (paired with cycle)
+    recurring = models.BooleanField(null=True, blank=True)  # yes/no
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True, related_name="subscriptions") # categories (id of that category)
     reminder = models.BooleanField(default=False)  # n/a for now
     created_at = models.DateTimeField(auto_now_add=True) # Created time when the subscription is created
